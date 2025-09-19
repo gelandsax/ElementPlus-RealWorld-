70e613283a3bbe2272bc1ea98ca3f23db5fdc21e
gelandsax <varstren@outlook.com>
2025-09-19 11:14:36 +0800

    1
    增加了显示tag的功能

--- vite-project/src/views/Home.vue
+++ vite-project/src/views/Home.vue
@@ -3,6 +3,13 @@
     <el-table-column prop="slug" label="slug" width="180" />
     <el-table-column prop="title" label="title" width="180" />
     <el-table-column prop="author.username" label="作者" />
+    <el-table-column label="Tag" width="180">
+      <template #default="scope">
+        <el-tag class="r-tag" v-for="tag in scope.row.tagList">{{
+          tag
+        }}</el-tag>
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
@@ -29,35 +36,37 @@ let article_info = ref<MultiArticles>({
 let isFeedActive = ref<boolean>(false);
 let isGlobalActive = ref<boolean>(true);
 
-function OnFeedClick(){
+function OnFeedClick() {
   isFeedActive.value = true;
   isGlobalActive.value = false;
 
-  axios.get('/articles/feed').then(res => {
+  axios.get("/articles/feed").then((res) => {
     article_info.value = res.data;
-  })
+  });
 }
 
-function OnGlobalClick(){
+function OnGlobalClick() {
   isFeedActive.value = false;
   isGlobalActive.value = true;
 
-  axios.get('/articles').then(res => {
+  axios.get("/articles").then((res) => {
     article_info.value = res.data;
-  })
+  });
 }
 
 onMounted(() => {
   axios.get("/tags").then((res) => {
     tag_info.value = res.data;
-  })
+  });
 
   axios.get("/articles").then((res) => {
     article_info.value = res.data;
-  })
-
-})
-
+  });
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+.r-tag {
+  margin: 0 6px;
+}
+</style>
