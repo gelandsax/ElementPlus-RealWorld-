2e8f20cdb8b8d143c4ed720680138df19ed6d10f
gelandsax <varstren@outlook.com>
2025-09-19 15:10:19 +0800

    2
    新增了分页组件，实现了分页和查询功能的组合

--- vite-project/src/views/AllArticle.vue
+++ vite-project/src/views/AllArticle.vue
@@ -12,7 +12,7 @@
         <el-option v-for="tag in tag_info.tags" :label="tag" :value="tag" />
       </el-select>
     </el-form-item>
-
+   
     <el-form-item>
       <el-button type="primary" @click.prevent="doQuery()">Query</el-button>
     </el-form-item>
@@ -25,68 +25,114 @@
 
     <el-table-column label="Tag" width="180">
       <template #default="scope">
-        <el-tag class="r-tag" v-for="tag in scope.row.tagList">{{
-          tag
-        }}</el-tag>
+        <el-tag class="r-tag" v-for="tag in scope.row.tagList">{{ tag }}</el-tag>
       </template>
     </el-table-column>
+
   </el-table>
+
+  <el-pagination
+      v-model:current-page="pageIndex"
+      v-model:page-size="pageSize"
+      :page-sizes="[5, 10, 15, 20]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="article_info.articlesCount"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import axios from "../utils/request";
-import type { MultiArticles, Tags } from "../models";
+import { onMounted, ref } from 'vue';
+import axios from '../utils/request'
+import type { MultiArticles, Tags } from '../models';
 
-interface QueryParam {
+
+const pageIndex = ref<number>(1)
+const pageSize = ref<number>(10)
+
+interface QueryParam{
   author: string;
   tag_name: string;
 }
 
 let query_param = ref<QueryParam>({
-  author: "",
-  tag_name: "",
-});
+  author: '',
+  tag_name: ''
+})
 
 let tag_info = ref<Tags>({
-  tags: [],
-});
+  tags: []
+})
 
 let article_info = ref<MultiArticles>({
   articles: [],
-  articlesCount: 0,
-});
+  articlesCount: 0
+})
 
-function doQuery(){
+const handleSizeChange = (val: number) => {
+  pageSize.value = val;
+  getArticles(pageIndex.value, pageSize.value, query_param.value.author,query_param.value.tag_name);
+  // console.log(`${val} items per page`)
+}
 
-  // 
+const handleCurrentChange = (val: number) => {
+  pageIndex.value = val;
+  getArticles(pageIndex.value, pageSize.value, query_param.value.author,query_param.value.tag_name);
+  // console.log(`current page: ${val}`)
+}
+
+function getArticles(pageIndex: number, pageSize: number, 
+author: string, tag_name:string)
+
+{
   let url = `/articles`;
-  if(query_param.value.tag_name!==''&&query_param.value.author!==''){
-    url += `?tag=${query_param.value.tag_name}&author=${query_param.value.author}`
+
+  let limit = pageSize;
+  let offset = (pageIndex - 1) * pageSize
+
+  if(tag_name!==''&&author!==''){
+    url += `?tag=${tag_name}&author=${author}&limit=${limit}&offset=${offset}`
   }
-  else if(query_param.value.tag_name!==''){
-    url += `?tag=${query_param.value.tag_name}`
+  else if(tag_name!==''){
+    url += `?tag=${tag_name}&limit=${limit}&offset=${offset}`
+  }
+  else if(author!==''){
+    url+=`?author=${author}&limit=${limit}&offset=${offset}`
   }
   else{
-    url+=`?author=${query_param.value.author}`
+    url+=`?limit=${limit}&offset=${offset}`
   }
+
+
+
 
   axios.get(url).then(res => {
     article_info.value = res.data;
-  })
+  });
+}
+
+function doQuery(){
+  pageIndex.value = 1; 
+  getArticles(pageIndex.value, pageSize.value, query_param.value.author, query_param.value.tag_name);
 }
 
 onMounted(() => {
-  axios.get("/tags").then((res) => {
+  axios.get('/tags').then(res => {
     tag_info.value = res.data;
-  });
+  })
 
-  axios.get("/articles").then((res) => {
-    article_info.value = res.data;
-  });
-});
+
+
+//   axios.get('/articles').then(res => {
+//     article_info.value = res.data;
+//   })
+  getArticles(pageIndex.value, pageSize.value, query_param.value.author, query_param.value.tag_name);
+})
+
+
 </script>
 <style scoped>
-.r-tag {
+.r-tag{
   margin: 0 6px;
 }
 .demo-form-inline .el-select {
